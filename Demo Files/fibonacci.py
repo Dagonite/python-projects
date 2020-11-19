f86617a9106fd3f8b71ab804615f50f0e812e087
Dagonite <up821535@myport.ac.uk>
2020-11-19 21:24:25 +0000

    Create fibonacci.py

---
+++ Demo Files/fibonacci.py
@@ -0,0 +1,14 @@
+################################################################################
+# fibonacci.py
+#
+# Program which prints out the Fibonacci sequence.
+################################################################################
+
+def fibonacci_sequence(limit):
+    a, b = 0, 1
+    for i in range(0, limit + 1):
+        print(a)
+        a, b = b, a + b
+
+
+fibonacci_sequence(100)
