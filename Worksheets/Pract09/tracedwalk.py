7a13398c11ce901780d491d181cbbc944dd3d532
Dagonite <up821535@myport.ac.uk>
2020-11-19 01:58:16 +0000

    PEP8 changes

--- Worksheets/Pract09/tracedwalk.py
+++ Worksheets/Pract09/tracedwalk.py
@@ -1,16 +1,16 @@
 from random import random
 
 
-'''6. Recall the one-dimensional random walk program from pract08 that 
-simulated several random walks in order to estimate the expected distance 
-away from the start point. Write a similar program tracedwalk.py that 
-simulates a single random walk that begins in the centre point of a pavement 
-that is n steps (or squares) long (the value of n should be input be the 
-user). The program should keep a count of how many times the walker steps 
-upon each square of the pavement. The walk ends when the walker steps off 
-either end of the pavement. For example, if the pavement is 5 squares long, 
-the walker begins on square 3. If the random walk takes steps forward, 
-backward, forward, forward, forward, then the program should report: 
+'''6. Recall the one-dimensional random walk program from pract08 that
+simulated several random walks in order to estimate the expected distance
+away from the start point. Write a similar program tracedwalk.py that
+simulates a single random walk that begins in the centre point of a pavement
+that is n steps (or squares) long (the value of n should be input be the
+user). The program should keep a count of how many times the walker steps
+upon each square of the pavement. The walk ends when the walker steps off
+either end of the pavement. For example, if the pavement is 5 squares long,
+the walker begins on square 3. If the random walk takes steps forward,
+backward, forward, forward, forward, then the program should report:
 
     Square      Steps
        1          0
@@ -19,6 +19,8 @@ backward, forward, forward, forward, then the program should report:
        4          2
        5          1
 '''
+
+
 def main():
     squares = get_inputs()
     steps, total_steps = simulate_steps(squares)
@@ -55,7 +57,7 @@ def simulate_steps(squares):
         if current_step > squares or current_step < 1:
             break
 
-        steps[current_step-1] += 1
+        steps[current_step - 1] += 1
         total_steps += 1
 
     return steps, total_steps
@@ -65,7 +67,7 @@ def print_steps(squares, steps, total_steps):
     print("\nA total of", total_steps, "steps were taken")
     print("\n{0:10}{1:1}".format("Square", "Steps"))
     for square in range(1, squares + 1):
-        print("{0:4}{1:9}".format(square, steps[square-1]))
+        print("{0:4}{1:9}".format(square, steps[square - 1]))
 
 
 main()
--- Worksheets/Pract09/tracedwalk2d.py
+++ Worksheets/Pract09/tracedwalk2d.py
@@ -2,15 +2,13 @@ import numpy as np
 from random import random
 
 
-'''7. [Harder] Write a two-dimensional version tracedwalk2d.py of 
-tracedwalk.py. This should simulate a single random walk of the form 
-considered in pract08, where the walker begins in the central square of a 
-square two dimensional grid of dimensions 9 by 9 steps, and where the walk 
-ends when the walker steps off the grid. Output the results in tabular form. 
+'''7. [Harder] Write a two-dimensional version tracedwalk2d.py of
+tracedwalk.py. This should simulate a single random walk of the form
+considered in pract08, where the walker begins in the central square of a
+square two dimensional grid of dimensions 9 by 9 steps, and where the walk
+ends when the walker steps off the grid. Output the results in tabular form.
 (Hint: you may need to use nested (two dimensional) lists.) '''
-# grid = [[0] * cols] * rows    # this makes a list with col times references
-                                # to the same list, grid[2][0] = 5 would make
-                                # all values in the first column 5
+
 
 def simulate_steps(cols, rows):
     grid = [[0] * cols for _ in range(rows)]
@@ -27,16 +25,12 @@ def simulate_steps(cols, rows):
             current_row += 1    # go down
         else:
             current_col -= 1    # go left
-            
+
         if current_row == -1 or current_row == rows \
-        or current_col == -1 or current_col == cols:
+                or current_col == -1 or current_col == cols:
             break
 
         grid[current_row][current_col] += 1
-
-    # this might come in helpful later on
-    # for i in range(rows):
-    #     grid[i] = [" " if j == 0 else j for j in grid[i]]
 
     grid = np.array(grid)
     print(grid)
