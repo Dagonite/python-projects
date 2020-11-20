1b122a353a861a6e02faa21cb17378e337a72b77
Dagonite <up821535@myport.ac.uk>
2020-11-20 15:59:06 +0000

    Update content_of_an_object.py

--- Demo Files/content_of_an_object.py
+++ Demo Files/content_of_an_object.py
@@ -1,3 +1,13 @@
-print(dir(str))
+################################################################################
+# content_of_an_object.py
+#
+# The dir() function function returns all properties and methods of the
+# specified object, without the values.
+#
+# The help() function is used to display the documentation of modules,
+# functions, classes, keywords etc.
+################################################################################
 
+print(dir(str))
+print()
 print(help(str.lower))
