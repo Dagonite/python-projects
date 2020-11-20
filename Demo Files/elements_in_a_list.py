5a4a39d541f63ab6614f1d7883670d393e04dcff
Dagonite <up821535@myport.ac.uk>
2020-11-20 15:43:28 +0000

    Update elements_in_a_list.py

--- Demo Files/elements_in_a_list.py
+++ Demo Files/elements_in_a_list.py
@@ -1,20 +1,24 @@
 ################################################################################
 # elements_in_a_list.py
 #
-# Program that shows that the following list alteration results in a row amount
-# of references to the same list
+# Program that shows that the first list results in a row amount of references
+# to the same element. The second list has unique references for each element in
+# the list
 ################################################################################
-
 
 from numpy import array
 
 
 cols, rows = 3, 6
+grid1 = [[0] * cols] * rows
+grid1[2][0] = 5
+grid1 = array(grid1)
+print(grid1)
 
-grid = [[0] * cols] * rows
+print()
 
-grid[2][0] = 5
+grid2 = [[0] * cols for _ in range(rows)]
+grid2[2][0] = 5
+grid2 = array(grid2)
 
-grid = array(grid)
-
-print(grid)
+print(grid2)
