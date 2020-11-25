75e263040c0bc67b78c08d33aa41075158b8cf77
Dagonite <up821535@myport.ac.uk>
2020-11-25 16:06:56 +0000

    Updated comments

--- Demo Files/map_function.py
+++ Demo Files/map_function.py
@@ -1,8 +1,8 @@
 ################################################################################
 # map_function.py
 #
-# Python includes (like Haskell) anonymous functions called lambda expressions.
-# The map function takes a function and a list, and gives a new list.
+# Python has anonymous functions called lambda expressions. The map function
+# takes a function and a list, and gives a new list.
 ################################################################################
 
 number_list = [0, 1, 2, 3, 4, 5, 6, 7, 9]
