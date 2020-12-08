a7f513762d241bbe6715d67368eb1bdcbc36e848
Dagonite <up821535@myport.ac.uk>
2020-12-08 17:22:07 +0000

    Added call_lift method

--- Worksheets/Pract10/lift.py
+++ Worksheets/Pract10/lift.py
@@ -1,19 +1,16 @@
 '''
 1.
 Using the Lift class created during the introduction, add an additional method
-which can be used to call the lift (call_lift(self, floor_called_from)). Note:
+which can be used to call the lift call_lift(self, floor_called_from). Note:
 your method should make use of both the go_up, and go_down methods
 '''
 
 
 class Lift:
     def __init__(self, max_floors):
-        # set the maximum number of floors (passes as a paramter)
         self.max_no_floors = max_floors
-        # set the current floor to the ground (always 0)
-        self.current_floor = 0
-        # tell the uesr which floor the lift is on
-        print("Current floor is: floor ", self.current_floor)
+        self.current_floor = 0      # ground floor is floor 0
+        print("Current floor is: floor", self.current_floor)
 
     def go_up(self, target_floor):
         if target_floor > self.max_no_floors:
@@ -23,7 +20,7 @@ class Lift:
         else:
             floors_to_move = target_floor - self.current_floor
             for i in range(floors_to_move):
-                print("Going up: floor number", self.current_floor)
+                print("Going up: floor", self.current_floor)
                 self.current_floor += 1
             print("The lift has arrived at floor", self.current_floor)
 
@@ -35,58 +32,19 @@ class Lift:
         else:
             floors_to_move = self.current_floor - target_floor
             for i in range(floors_to_move):
-                print("Going down: floro number", self.current_floor)
+                print("Going down: floor", self.current_floor)
                 self.current_floor -= 1
             print("The lift has arrived at floor", self.current_floor)
 
+    def call_lift(self, floor_called_from):
+        if floor_called_from > self.current_floor:
+            self.go_up(floor_called_from)
+        elif floor_called_from == self.current_floor:
+            print("Already on floor", floor_called_from)
+        else:
+            self.go_down(floor_called_from)
+
 
 lift1 = Lift(10)
-lift2 = Lift(6)
-print()
-
-print("Lift 1 maximum number floors:", lift1.max_no_floors)
-print("Lift 2 maximum number floors:", lift2.max_no_floors, "\n")
-
-lift1.go_up(4)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_up(11)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_up(2)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_up(8)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_down(-5)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_down(5)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift1.go_down(8)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift2.go_up(4)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift2.go_up(11)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift2.go_up(2)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
-
-lift2.go_down(-5)
-print("Lift 1 current floor:", lift1.current_floor)
-print("Lift 2 current floor:", lift2.current_floor, "\n")
+print("Lift called to floor 5")
+lift1.call_lift(5)
