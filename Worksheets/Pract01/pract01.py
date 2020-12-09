1eaea676f4ae42bf6a036268d4f491da233d631b
Dagonite <up821535@myport.ac.uk>
2020-12-09 19:01:11 +0000

    Corrected print statement

--- Worksheets/Pract01/pract01.py
+++ Worksheets/Pract01/pract01.py
@@ -1,5 +1,6 @@
 # Practical Worksheet 1: Getting started with Python
 
+
 def say_hello1():
     print("Hello world")
 
@@ -15,19 +16,19 @@ def kilos_to_pounds():
     print("The weight in pounds is", pounds)
 
 
-'''1. Write a function called say_name that displays your name. '''
+"""1. Write a function called say_name that displays your name. """
 
 
 def say_name():
     print("John Doe")
 
 
-'''2. Write a say_hello2 function that uses two print statements to display the
+"""2. Write a say_hello2 function that uses two print statements to display the
 text:
 
     Hello
     World
-'''
+"""
 
 
 def say_hello2():
@@ -35,9 +36,9 @@ def say_hello2():
     print("World")
 
 
-'''3. Write a function euros_to_pounds which converts an amount in Euros entered
+"""3. Write a function euros_to_pounds which converts an amount in Euros entered
 by the user to a corresponding amount in Pounds. Assume that the exchange rate
-is 1.15 Euros to the Pound. (Hint: be sure to test your solution carefully.) '''
+is 1.15 Euros to the Pound. (Hint: be sure to test your solution carefully.) """
 
 
 def euros_to_pounds():
@@ -46,8 +47,8 @@ def euros_to_pounds():
     print("The amount in pounds is", pounds)
 
 
-'''4. Write an add_up function that asks the user to enter two numbers,
-and outputs their sum. '''
+"""4. Write an add_up function that asks the user to enter two numbers,
+and outputs their sum. """
 
 
 def add_up():
@@ -57,10 +58,10 @@ def add_up():
     print("The sum of them is", total)
 
 
-'''5. Write a change_counter function. This should ask the user how many 1p, 2p
+"""5. Write a change_counter function. This should ask the user how many 1p, 2p
 and 5p coins they have (using three separate questions), and then display the
 total amount of money in pence. (Hint: remember that variable names cannot begin
-with a digit, so you might like to use names like two_pence.) '''
+with a digit, so you might like to use names like two_pence.) """
 
 
 def change_counter():
@@ -71,8 +72,8 @@ def change_counter():
     print("The sum of your coins is", total_pence, "pence")
 
 
-'''6. Write a ten_hellos function that uses a loop to display “hello, world!”
-ten times (on separate lines). '''
+"""6. Write a ten_hellos function that uses a loop to display “hello, world!”
+ten times (on separate lines). """
 
 
 def ten_hellos():
@@ -80,9 +81,9 @@ def ten_hellos():
         print("hello, world")
 
 
-'''7. Change the count function so that instead of counting from 0 to 9,
+"""7. Change the count function so that instead of counting from 0 to 9,
 it counts up from 1 to 10. (Hint: Use a little arithmetic in the print
-statement.) '''
+statement.) """
 
 
 def count2():
@@ -90,9 +91,9 @@ def count2():
         print(i + 1)
 
 
-'''8. [harder] Write a function weights_table that outputs a two-column table
+"""8. [harder] Write a function weights_table that outputs a two-column table
 of kilogram weights and their pound equivalents for kilogram values 0, 10,
-20 ... 100. (Don’t worry too much about formatting the table neatly.) '''
+20 ... 100. (Don’t worry too much about formatting the table neatly.) """
 
 
 def weights_table():
@@ -100,19 +101,21 @@ def weights_table():
         print(i * 10, i * 22)
 
 
-'''9. [harder] Write a future_value function that uses a loop to calculate
+"""9. [harder] Write a future_value function that uses a loop to calculate
 the future value of an investment amount, assuming an annual interest rate of
 5.5%. The function should ask the user for the initial amount and the number
 of years that it is to be invested, and should output the final value of the
-investment using compound interest with the interest compounded every year. '''
+investment using compound interest with the interest compounded every year. """
 
 
 def future_value():
     investment = eval(input("Enter the investment amount in GBP: "))
     years = eval(input("Enter the years of investment: "))
-    interest_rate = .055
+    interest_rate = 0.055
     return_amount = investment
     for year in range(years):
-        return_amount *= (1 + interest_rate)
-    print("Investing £{:.2f} for {} years".format(investment, years),
-          "will result in £{:.2f}.".format(return_amount))
+        return_amount *= 1 + interest_rate
+    print(
+        "Investing £{:.2f} for {} years".format(investment, years),
+        "will result in £{:.2f}".format(return_amount),
+    )
