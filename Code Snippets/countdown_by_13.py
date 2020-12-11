cf487cf2c73ce93b43eb6eab400b21bc1edcc115
Dagonite <up821535@myport.ac.uk>
2020-12-11 00:49:36 +0000

    Moved non project files to separate repo

--- Code Snippets/countdown_by_13.py
+++
@@ -1,5 +0,0 @@
-# countdown_by_13.py
-# countdown from 700 to 200 in decrements of 13
-
-for n in range(700, 200, -13):
-    print(n)
--- FizzBuzz/one_line_fizzbuzz.py
+++
@@ -1,3 +0,0 @@
-# one_line_fizzbuzz.py
-
-{print("Fizz" * (n % 3 < 1) + "Buzz" * (n % 5 < 1) or n) for n in range(1, 101)}
