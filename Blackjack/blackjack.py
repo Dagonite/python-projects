192869c5cb2d7f4c1e299997cca5a63c56cb6225
Dagonite <up821535@myport.ac.uk>
2021-03-23 23:05:37 +0000

    Redo blackjack game
    Replace blackjack game with version that doesn't require the graphics module

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,41 +1,12 @@
-from random import shuffle
+import random
 
-RANKS = (
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "Jack",
-    "Queen",
-    "King",
-    "Ace",
-)
-SUITS = (
-    "Diamonds",
-    "Hearts",
-    "Clubs",
-    "Spades",
-)
-VALUES = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "Jack": 10,
-    "Queen": 10,
-    "King": 10,
-    "Ace": 11,
-}
+# set up the constants
+RANKS = tuple(range(2, 11)) + ("Jack", "Queen", "King", "Ace")
+DIAMONDS = chr(9830)
+HEARTS = chr(9829)
+CLUBS = chr(9827)
+SPADES = chr(9824)
+SUITS = (DIAMONDS, HEARTS, CLUBS, SPADES)
 
 
 class Card:
@@ -43,26 +14,29 @@ class Card:
         self.suit = suit
         self.rank = rank
 
-    def __str__(self):
-        return self.rank + " of " + self.suit
+    def format_card(self, rows, hidden=False):
+        rows[0] += " ___  "  # top line of a card
+        if hidden:
+            # backside of a card
+            rows[1] += "|## | "
+            rows[2] += "|###| "
+            rows[3] += "|_##| "
+        else:
+            # frontside of a card
+            rows[1] += "|{} | ".format(self.rank.ljust(2))
+            rows[2] += "| {} | ".format(self.suit)
+            rows[3] += "|_{}| ".format(self.rank.rjust(2, "_"))
 
 
 class Deck:
     def __init__(self):
-        self.deck = [Card(suit, rank) for rank in RANKS for suit in SUITS]
+        self.deck = [Card(suit, str(rank)[0]) if rank != 10 else Card(suit, "10") for rank in RANKS for suit in SUITS]
 
-    def __str__(self):
-        deck_comp = ""
-        for card in self.deck:
-            deck_comp += "\n " + card.__str__()
-        return "The deck has: " + deck_comp
-
-    def shuffle(self):
-        shuffle(self.deck)
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
 
     def deal(self):
-        single_card = self.deck.pop()
-        return single_card
+        return self.deck.pop()
 
 
 class Hand:
@@ -71,10 +45,22 @@ class Hand:
         self.value = 0
         self.aces = 0
 
+    def format_hand(self, hidden=False):
+        rows = ["", "", "", "", ""]
+        for card in self.cards:
+            card.format_card(rows, hidden)
+            hidden = False
+        for row in rows:
+            print(row)
+
     def add_card(self, card):
         self.cards.append(card)
-        self.value += VALUES[card.rank]
-        if card.rank == "Ace":
+        if card.rank.isnumeric():
+            self.value += int(card.rank)
+        elif card.rank in ("J", "Q", "K"):
+            self.value += 10
+        elif card.rank == "A":
+            self.value += 11
             self.aces += 1
 
     def adjust_for_ace(self):
@@ -84,15 +70,23 @@ class Hand:
 
 
 class Chips:
-    def __init__(self):
-        self.total = 100
+    def __init__(self, total):
+        self.total = total
         self.bet = 0
 
-    def take_bet(self):
-        self.total -= self.bet
+    def __str__(self):
+        return str(self.total)
+
+    def take_bet(self, bet):
+        self.bet += bet
+        self.total -= bet
 
     def win_bet(self):
         self.total += self.bet * 2
+        return self.bet
 
     def draw_bet(self):
         self.total += self.bet
+
+    def clear_bet(self):
+        self.bet = 0
--- Blackjack/play_blackjack.py
+++ Blackjack/play_blackjack.py
@@ -1,386 +1,203 @@
-# play_blackjack.py
-"""Program which allows a player to play blackjack with a computer dealer within a graphical user interface.
+import sys
 
-Rules:
--dealer must hit below 17 if player isn't bust
--dealer must stand when they reach 17.
--minimum bet of 1 chip
--maximum bet of 100 chips
--no card counting :^)
-"""
+from blackjack import RANKS, DIAMONDS, HEARTS, CLUBS, SPADES, SUITS, Card, Deck, Hand, Chips
 
-from time import sleep
 
-from graphics import Circle, Entry, GraphWin, Line, Point, Polygon, Rectangle, Text
-
-from blackjack import Card, Chips, Deck, Hand
-
-card_objects = []
+START_CHIPS = 1000
 
 
 def main():
-    global player_chips
-    player_chips = Chips()
-    win = create_interface()
-    while True:
-        make_bet(win)
-        result = play_game(win)
+    print(
+        """\nA game of Blackjack
 
-        if result == 2:
-            player_chips.win_bet()
-        elif result == 1:
-            player_chips.draw_bet()
+    Rules:
+      Try to beat the dealer's hand by without going over 21.
+      Cards 2 through 10 are worth their face value.
+      Kings, Queens, and Jacks are worth 10 points. 
+      Each Ace is worth 11 points but are reduced to 1 individually if the hand goes over 21.
+      (H)it to take another card. (S)tand to stop taking cards.
+      On the first play of a round, you can (D)ouble down to increase your bet.
+      By doubling down you commit to one hit followed by a stand (assuming you don't go over 21).
+      In case of a tie, the bet is returned to the player.
+      The dealer must hit until they reach 17 or over.\n"""
+    )
 
-        for i in card_objects:
-            i.undraw()
+    # give the player their starting chips
+    player_chips = Chips(START_CHIPS)
 
-        chips_text.setText("Chips: " + str(player_chips.total))
-        helper_text.setText("Enter a bet to play")
+    while True:  # main game loop
+        if player_chips.total < 1:  # check if the player has run out of chips
+            print("\nYou've run out of chips. Thanks for playing!")
+            sys.exit()
+
+        # prompt the player for their intitial bet
+        bet = get_bet(player_chips)
+
+        # get and shuffle a deck of cards
+        deck = get_deck()
+
+        # give the dealer and player two cards from the deck each
+        dealer_hand, player_hand = deal_initial_hands(deck)
+
+        # prompt the player for moves
+        player_is_bust = process_player_moves(deck, dealer_hand, player_hand, player_chips, bet)
+
+        print()
+
+        if player_is_bust:  # check if the player has bust
+            print("You've gone over 21.")
+            print(f"You've lost {player_chips.bet} chip(s).")
+        else:  # player hasn't bust
+
+            # reveal dealer's hidden card
+            display_hands(deck, (dealer_hand, player_hand), False)
+
+            # await for user input beforing proceeding
+            input("Press Enter to continue... ")
+
+            # process the dealer's moves
+            dealer_is_bust = process_dealer_moves(deck, dealer_hand, player_hand)
+
+            print()
+
+            if dealer_is_bust:  # check if the dealer has bust
+                print("The dealer has gone over 21.")
+                print(f"You've won {player_chips.win_bet()} chip(s).")
+            else:
+                if player_hand.value > dealer_hand.value:  # check if player hand beats dealer's
+                    print("You have the better hand.")
+                    print(f"You've won {player_chips.win_bet()} chip(s).")
+                elif player_hand.value == dealer_hand.value:  # check if the hands are equal
+                    print("Both hands are equal. It's a draw.")
+                    player_chips.draw_bet()
+                    print("Your chips have been returned.")
+                else:  # otherwise dealer's hand is better
+                    print("Dealer has the better hand.")
+                    print(f"You've lost {player_chips.bet} chip(s).")
+
+        print()
+
+        # clear the player's current bet
+        player_chips.clear_bet()
 
 
-def make_bet(win):
-    while True:
-        if player_chips.total < 1:
-            helper_text.setText("Player out of chips\n GAME OVER")
-            sleep(4)
-            win.close()
-        cursor = win.getMouse()
-        if 3.6 <= cursor.getX() <= 4.1 and 0.15 <= cursor.getY() <= 0.45:
-            bet = bid_entry.getText()
-            if validate_bet(bet):
-                break
-        elif 9.3 <= cursor.getX() <= 9.9 and 0.15 <= cursor.getY() <= 0.45:
-            win.close()
-    bid_entry.setText("")
-
-
-def validate_bet(bet):
-    if int(bet) > player_chips.total:
-        helper_text.setText("Bet can't be higher\nthan total chips")
-        return False
-    elif int(bet) > 100:
-        helper_text.setText("Bet can't be above 100 chips")
-        return False
-    elif int(bet) < 1:
-        helper_text.setText("Bet can't be less than 1 chip")
-        return False
-    else:
-        player_chips.bet = int(bet)
-        player_chips.take_bet()
-        # player_chips.total -= player_chips.bet
-        chips_text.setText("Chips: " + str(player_chips.total))
-        return True
-
-
-def play_game(win):
-    helper_text.setText("Choose whether you want to hit or stand")
-
+def get_deck():
+    """Return a Deck object containing 52 Card objects."""
+    # create deck object
     deck = Deck()
-    deck.shuffle()
 
-    player_hand = Hand()
-    hit(deck, player_hand)
-    hit(deck, player_hand)
+    # shuffle the deck
+    deck.shuffle_deck()
 
+    return deck
+
+
+def deal_initial_hands(deck):
+    """Return the initial hands of the player and dealer containing two cards each."""
+    # draw two cards for the dealer
     dealer_hand = Hand()
     hit(deck, dealer_hand)
     hit(deck, dealer_hand)
 
-    for card_index, player_card in enumerate(player_hand.cards):
-        card_name = str(player_card)
-        suit = check_suit(card_name)
-        draw_card(win, card_index, suit, card_name[0], True)
+    # draw two cards for the player
+    player_hand = Hand()
+    hit(deck, player_hand)
+    hit(deck, player_hand)
 
-    show_card = True
-    for dealer_card_index, dealer_card in enumerate(dealer_hand.cards):
-        card_name = str(dealer_card)
-        suit = check_suit(card_name)
+    # show the cards
+    display_hands(deck, (dealer_hand, player_hand))
 
-        if show_card:
-            draw_card(win, dealer_card_index, suit, card_name[0], False)
-        else:
-            draw_hidden_card(win, dealer_card_index, suit, card_name[0], False)
-
-        show_card = False
-
-    player_bust = hit_or_stand(win, deck, player_hand, card_index)
-
-    if not player_bust:
-        helper_text.setText(f"Player stands on {player_hand.value}")
-        sleep(3)
-        dealer_bust = dealer_hit_or_stand(
-            win, deck, dealer_hand, dealer_card_index, player_hand
-        )
-        if not dealer_bust:
-            if dealer_hand.value > player_hand.value:
-                helper_text.setText(
-                    f"Dealer beats player {dealer_hand.value} to {player_hand.value}"
-                )
-                sleep(4)
-                return 0
-            elif dealer_hand.value < player_hand.value:
-                helper_text.setText(
-                    f"Player beats Dealer {player_hand.value} to {dealer_hand.value}"
-                )
-                sleep(4)
-                return 2
-            else:
-                helper_text.setText(
-                    f"Dealer and Player both have {player_hand.value}\nIt's a push, chips returned"
-                )
-                sleep(4)
-                return 1
-        else:
-            helper_text.setText(f"Dealer bust, Player wins with {player_hand.value}")
-            sleep(4)
-            return 2
-    else:
-        helper_text.setText(f"Player bust, Dealer wins")
-        sleep(4)
-        return 0
-
-
-def check_suit(card_name):
-    if "Diamonds" in card_name:
-        return "♦"
-    elif "Hearts" in card_name:
-        return "♥"
-    elif "Clubs" in card_name:
-        return "♣"
-    else:
-        return "♠"
-
-
-def hit_or_stand(win, deck, player_hand, card_index):
-    player_bust = False
-
-    while player_hand.value < 21:
-        cursor = win.getMouse()
-        if 0.4 <= cursor.getX() <= 1.3 and 0.15 <= cursor.getY() <= 0.45:
-            hit(deck, player_hand)
-            # player_hand.add_card(deck.deal())
-            card_index += 1
-            card_name = str(player_hand.cards[card_index])
-            suit = check_suit(card_name)
-
-            helper_text.setText(f"You drew the {card_name}")
-            draw_card(win, card_index, suit, card_name[0], True)
-
-        elif 1.4 <= cursor.getX() <= 2.3 and 0.15 <= cursor.getY() <= 0.45:
-            helper_text.setText("You have chosen to stand")
-            break
-
-        elif 9.3 <= cursor.getX() <= 9.9 and 0.15 <= cursor.getY() <= 0.45:
-            win.close()
-
-    if player_hand.value > 21:
-        player_bust = True
-
-    return player_bust
+    return dealer_hand, player_hand
 
 
 def hit(deck, hand):
+    """Add a card to the supplied hand by taking a card from the deck."""
     hand.add_card(deck.deal())
     hand.adjust_for_ace()
 
 
-def dealer_hit_or_stand(win, deck, dealer_hand, dealer_card_index, player_hand):
-    dealer_bust = False
-    card_name = str(dealer_hand.cards[1])
-    suit = check_suit(card_name)
-    draw_card(win, 1, suit, card_name[0], False)
+def display_hands(deck, hands, hidden=True):
+    hidden_value = "??" if hidden else hands[0].value
+    print(f"\nDEALER: {hidden_value}")
+    hands[0].format_hand(hidden)
 
-    if dealer_hand.value <= player_hand.value:
-
-        while dealer_hand.value < 17:
-            sleep(1.5)
-            hit(deck, dealer_hand)
-            dealer_card_index += 1
-            card_name = str(dealer_hand.cards[dealer_card_index])
-            suit = check_suit(card_name)
-
-            draw_card(win, dealer_card_index, suit, card_name[0], False)
-
-    if dealer_hand.value > 21:
-        dealer_bust = True
-
-    return dealer_bust
+    print(f"PLAYER: {hands[1].value}")
+    hands[1].format_hand()
 
 
-def draw_text(
-    win,
-    centre,
-    txt,
-    text_size=14,
-    colour="black",
-    style="normal",
-    face="courier",
-    card_shape="False",
-):
-    current_text = Text(Point(centre[0], centre[1]), txt)
-    current_text.setFace(face)
-    current_text.setSize(text_size)
-    current_text.setTextColor(colour)
-    current_text.setStyle(style)
-    current_text.draw(win)
-    if card_shape:
-        return current_text
+def get_bet(player_chips, initial_bet=None):
+    # determine the maximum chips the player can bet
+    # if there has already been an initial bet: use the minimum of the initial bet and the player's total chips
+    # else: use the player's total chips
+    max_bet = min(initial_bet, (player_chips.total)) if initial_bet is not None else player_chips.total
+
+    while True:  # loop until the player gives a valid bet
+        bet = input(f"Enter an amount to bet (1-{max_bet}, or (Q)uit)> ").upper().strip()
+
+        if bet == "Q":
+            print("Thanks for playing!")
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue  # if the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= max_bet:
+            player_chips.take_bet(bet)
+            return bet  # player entered a valid bet.
 
 
-def create_interface():
-    win = GraphWin("Blackjack", 1000, 500)
-    win.setBackground("green")
-    win.setCoords(0, 0, 10, 5)
+def process_player_moves(deck, dealer_hand, player_hand, player_chips, bet):
+    move = ""
+    while player_hand.value < 22:  # loop whilst player hasn't bust
+        if player_hand.value == 21 or move == "D":
+            return False  # automatically stand for the player if they have 21 or doubled down
 
-    # draw player interface
-    draw_rectangle(win, (0, 0), (10, 0.6), "black")
+        # get the player's move
+        move = get_move(player_hand, player_chips)
 
-    # draw player's hand text
-    draw_text(win, (1.1, 0.76), "Player's hand", 14, "black", "bold")
+        if move == "H":
+            hit(deck, player_hand)  # add a card to the player's hand
+        elif move == "S":
+            return False  # player decides to stand
+        elif move == "D":
+            # player has doubled down so get another bet
+            bet = get_bet(player_chips, bet)
 
-    # draw player's chips text
-    global chips_text
-    chips_text = Text(Point(9.0, 0.76), "Chips: 100")
-    chips_text.setFace("courier")
-    chips_text.setSize(14)
-    chips_text.setStyle("bold")
-    chips_text.draw(win)
+            # add a card to the player's hand
+            hit(deck, player_hand)
 
-    # draw dealer's hand text
-    draw_text(win, (1.1, 4.82), "Dealer's hand", 14, "black", "bold")
+        # show the cards
+        display_hands(deck, (dealer_hand, player_hand))
 
-    # draw hit button
-    draw_rectangle(win, (0.4, 0.15), (1.3, 0.45), "grey")
-    draw_text(win, (0.85, 0.29), "Hit", 14, "black", "bold", "arial")
-
-    # draw stand button
-    draw_rectangle(win, (1.4, 0.15), (2.3, 0.45), "grey")
-    draw_text(win, (1.85, 0.29), "Stand", 14, "black", "bold", "arial")
-
-    # draw bet entry point
-    global bid_entry
-    bid_entry = Entry(Point(3, 0.3), 10)
-    bid_entry.draw(win)
-
-    # draw bet button
-    draw_rectangle(win, (3.6, 0.15), (4.1, 0.45), "green")
-    draw_text(win, (3.84, 0.29), "Bet", 14, "black", "bold", "arial")
-
-    # draw helper text
-    global helper_text
-    helper_text = Text(Point(6.7, 0.29), "Enter a bet to play")
-    helper_text.setFace("arial")
-    helper_text.setSize(14)
-    helper_text.setTextColor("white")
-    helper_text.setStyle("bold")
-    helper_text.draw(win)
-
-    # draw quit button
-    draw_rectangle(win, (9.3, 0.15), (9.9, 0.45), "red")
-    draw_text(win, (9.6, 0.29), "Quit", 14, "black", "bold", "arial")
-
-    return win
+    return True
 
 
-def draw_card(win, offset, suit, rank, player):
-    if rank == "1":
-        rank = "1" + "0"
+def get_move(player_hand, player_chips):
+    while True:  # loop until the player gives a valid answer
+        moves = ["(H)it", "(S)tand"]
 
-    if player:
-        vertical_offset = 0
-    else:
-        vertical_offset = 2
+        if len(player_hand.cards) == 2 and player_chips.total > 0:
+            moves.append("(D)ouble down")
 
-    card_rectangle = draw_rectangle(
-        win,
-        (0.4 + offset, 0.95 + vertical_offset),
-        (1.8 + offset, 2.6 + vertical_offset),
-        "white",
-        True,
-    )
-    card_objects.append(card_rectangle)
+        # prompt the player for a move
+        move_prompt = ", ".join(moves) + "> "
+        move = input(move_prompt).upper()
 
-    if suit in ("♦", "♥"):
-        colour = "red"
-    else:
-        colour = "black"
-
-    card_shape1 = draw_text(
-        win,
-        (0.6 + offset, 2.4 + vertical_offset),
-        rank,
-        18,
-        colour,
-        "bold",
-        "courier",
-        True,
-    )
-    card_shape2 = draw_text(
-        win,
-        (0.6 + offset, 1.12 + vertical_offset),
-        suit,
-        30,
-        colour,
-        "bold",
-        "courier",
-        True,
-    )
-    card_shape3 = draw_text(
-        win,
-        (1.6 + offset, 1.12 + vertical_offset),
-        rank,
-        18,
-        colour,
-        "bold",
-        "courier",
-        True,
-    )
-    card_shape4 = draw_text(
-        win,
-        (1.6 + offset, 2.4 + vertical_offset),
-        suit,
-        30,
-        colour,
-        "bold",
-        "courier",
-        True,
-    )
-    card_objects.append(card_shape1)
-    card_objects.append(card_shape2)
-    card_objects.append(card_shape3)
-    card_objects.append(card_shape4)
+        if move in ("H", "S") or (move == "D" and "(D)ouble down" in moves):
+            return move  # player has entered a valid move.
 
 
-def draw_hidden_card(win, offset, suit, rank, player):
-    if rank == "1":
-        rank = "1" + "0"
+def process_dealer_moves(deck, dealer_hand, player_hand):
+    while dealer_hand.value < 17:  # dealer must hit until they have at least 17
+        hit(deck, dealer_hand)
+        display_hands(deck, (dealer_hand, player_hand), False)
 
-    if player:
-        vertical_offset = 0
-    else:
-        vertical_offset = 2
+        # await for user input beforing proceeding
+        input("Press Enter to continue... ")
 
-    card_rectangle = draw_rectangle(
-        win,
-        (0.4 + offset, 0.95 + vertical_offset),
-        (1.8 + offset, 2.6 + vertical_offset),
-        "blue",
-        True,
-    )
-    card_objects.append(card_rectangle)
-
-
-def draw_rectangle(win, point1, point2, colour, card_object="False"):
-    current_rectangle = Rectangle(
-        Point(point1[0], point1[1]),
-        Point(point2[0], point2[1]),
-    )
-    current_rectangle.setFill(colour)
-    current_rectangle.setWidth(2)
-    current_rectangle.draw(win)
-
-    if card_object:
-        return current_rectangle
+    # return True if dealer has bust
+    return True if dealer_hand.value > 21 else False
 
 
 if __name__ == "__main__":
