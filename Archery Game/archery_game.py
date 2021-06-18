c52661a9ac9503d17104bfc062db9e61874d7af5
Dagonite <up821535@myport.ac.uk>
2021-06-18 12:36:13 +0100

    Functionalise program more
    -move constants out of functions
    -capitalise constants
    -add comments and docstrings

--- Archery Game/archery_game.py
+++ Archery Game/archery_game.py
@@ -1,5 +1,5 @@
-"""Game where a user has to shoot arrows at a target. The arrows are offset by wind so the user has to take this into 
-account before each shot. After firing all arrows the user gets a score and a grade."""
+"""Game where a user gets to shoot arrows at a target. The arrows are offset by wind so this needs to be taken into 
+account before each shot. After shooting all arrows a score and a grade are given."""
 
 import random
 
@@ -16,16 +16,40 @@ GRADES = [
     "awful at this game",
 ]
 
+WSD = 0.25  # wind start deviation
+WD = 0.1  # max wind deviation per subsequent arrow
+WTH = 0.08  # wind threshold (determines what direction the wind is going)
+ARROWS = 5  # number of arrow to shoot
+
 
 def archery_game():
+    win, wind_text, zone_text, score_text, score = draw_gui()
+    score = shoot_arrows(win, wind_text, zone_text, score_text, score)
+    give_grade(wind_text, score_text, score)
+
+    # wait for user to click on the screen
+    cursor = win.getMouse()
+    cursor_x, cursor_y = cursor.getX(), cursor.getY()
+    win.close()
+
+    # if the user clicked on the target, reload the window
+    if distance_between_points(Point(cursor_x, cursor_y), Point(0.5, 0.5)) <= 0.3:
+        archery_game()
+
+
+def draw_gui():
+    """Create the graphics window and draw the target."""
+    # create the window
     win = GraphWin("Archery game", 500, 500)
     win.setBackground("cyan")
     win.setCoords(0, 0, 1, 1)
     centre = Point(0.5, 0.5)
 
+    # draw the grass
     ground_rect = Rectangle(Point(-0.01, -0.01), Point(1.01, 0.5)).draw(win)
     ground_rect.setFill("green")
 
+    # draw the target legs
     left_target_stand = Polygon(Point(0.02, -0.01), Point(0.45, 0.5), Point(0.55, 0.5), Point(0.12, -0.01)).draw(win)
     left_target_stand.setFill("brown")
     left_target_stand.setWidth(2)
@@ -34,57 +58,64 @@ def archery_game():
     right_target_stand.setFill("brown")
     right_target_stand.setWidth(2)
 
+    # draw the target
     draw_circle(win, centre, 0.3, "blue")
     draw_circle(win, centre, 0.2, "red")
     draw_circle(win, centre, 0.1, "yellow")
 
+    # draw the text displaying the score
     score = 0
-    score_text = Text(Point(0.5, 0.04), "Score: {}".format(score)).draw(win)
+    score_text = Text(Point(0.5, 0.04), f"Score: {score}").draw(win)
     score_text.setSize(18)
     score_text.setStyle("bold")
 
+    # draw the text displaying where an arrow has landed
     zone_text = Text(Point(0.5, 0.09), "").draw(win)
     zone_text.setSize(12)
     zone_text.setStyle("bold")
 
-    wind_text = Text(Point(0.5, 0.97), ("Wind: ")).draw(win)
+    # draw the text describing wind conditions
+    wind_text = Text(Point(0.5, 0.97), "Wind: ").draw(win)
     wind_text.setSize(12)
     wind_text.setStyle("bold")
 
-    wsd = 0.25  # wind start deviation
-    wd = 0.1  # wind deviation per arrow
-    wth = 0.08  # wind threshold
-    arrows = 5  # number of arrows to shoot
+    return win, wind_text, zone_text, score_text, score
 
-    h_wind, v_wind = random.uniform(-wsd, wsd), random.uniform(-wsd, wsd)
 
-    for _ in range(arrows):
+def shoot_arrows(win, wind_text, zone_text, score_text, score):
+    """Prompt user to shoot arrows at the target by left-clicking."""
+    h_wind, v_wind = random.uniform(-WSD, WSD), random.uniform(-WSD, WSD)
+
+    for _ in range(ARROWS):
+        # determine wind direction
         wind = ""
-
-        if v_wind > wth:
+        if v_wind > WTH:
             wind += "north"
-        elif v_wind < -wth:
+        elif v_wind < -WTH:
             wind += "south"
-
-        if h_wind > wth:
+        if h_wind > WTH:
             wind += "east"
-        elif h_wind < -wth:
+        elif h_wind < -WTH:
             wind += "west"
-
         if wind == "":
             wind = "Calm"
 
-        wind_text.setText("Wind: {}".format(wind.title()))
+        # display wind direction
+        wind_text.setText(f"Wind: {wind.title()}")
 
+        # wait for left-click
         cursor_pos = win.getMouse()
-        arrow_x = cursor_pos.getX() + h_wind
-        arrow_y = cursor_pos.getY() + v_wind
+        arrow_x = cursor_pos.getX() + h_wind  # add horizontal wind to x value
+        arrow_y = cursor_pos.getY() + v_wind  # add vertical wind to y value
 
-        h_wind += random.uniform(-wd, wd)
-        v_wind += random.uniform(-wd, wd)
+        # add wind deviation for next arrow shot
+        h_wind += random.uniform(-WD, WD)
+        v_wind += random.uniform(-WD, WD)
 
+        # get the distance from the target centre of where the arrow will land
         arrow_zone = distance_between_points(Point(arrow_x, arrow_y), Point(0.5, 0.5))
 
+        # determine what zone the arrow will land in
         if arrow_zone <= 0.3:
             draw_arrow(win, arrow_x, arrow_y)
             if arrow_zone < 0.1:
@@ -99,15 +130,20 @@ def archery_game():
         else:
             zone_text.setText("TARGET MISSED (0 POINTS!)")
 
-        score_text.setText("Score: {}".format(score))
+        score_text.setText(f"Score: {score}")
 
-    if score > 220 / arrows:
+    return score
+
+
+def give_grade(wind_text, score_text, score):
+    """Display what score and grade the user has received."""
+    if score > 220 / ARROWS:
         grade = 0
-    elif score > 170 / arrows:
+    elif score > 170 / ARROWS:
         grade = 1
-    elif score > 120 / arrows:
+    elif score > 120 / ARROWS:
         grade = 2
-    elif score > 95 / arrows:
+    elif score > 95 / ARROWS:
         grade = 3
     else:
         grade = 4
@@ -118,16 +154,9 @@ def archery_game():
     score_text.setSize(10)
     score_text.setText("Click on the target to play again\nClick anywhere else to quit")
 
-    choice = win.getMouse()
-    choice_x, choice_y = choice.getX(), choice.getY()
-
-    win.close()
-
-    if distance_between_points(Point(choice_x, choice_y), Point(0.5, 0.5)) <= 0.3:
-        archery_game()
-
 
 def draw_arrow(win, arrow_x, arrow_y):
+    """Draw arrow onto the given graphics window."""
     arrow_shaft = Circle(Point(arrow_x, arrow_y), 0.008).draw(win)
     arrow_shaft.setFill("brown")
 
