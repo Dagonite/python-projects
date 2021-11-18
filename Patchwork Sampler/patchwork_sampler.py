f8e14b92330758fb50cc575aec8771eb4325c81f
Dagonite <samuel.stock@stfc.ac.uk>
2021-11-18 12:51:09 +0000

    Fix docstring

--- Patchwork Sampler/patchwork_sampler.py
+++ Patchwork Sampler/patchwork_sampler.py
@@ -276,9 +276,8 @@ def redraw_shapes(win, size, colour, col, row, tiles):
     Determine what tile design needs to be drawn then call the relevant design
     function with the next colour for that tile.
 
-    If either of the supplied vectors point to a perimeter of the patchwork then
-    they must pointing to a net patch. Otherwise they must be pointing to a
-    circle patch.
+    If the supplied vectors point to a perimeter tile, draw the net design.
+    Otherwise draw the circle design.
     """
     if any(vector in (1, size) for vector in (col, row)):
         net_design(win, size, colour, col, row, tiles)
