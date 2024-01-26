56ecf6223b3c17ef99e721af3ad0b06e67884f4f
Dagonite <samuel.stock@stfc.ac.uk>
2024-01-26 06:06:02 +0000

    Move code into a function

--- Rock Paper Scissors/rock_paper_scissors.py
+++ Rock Paper Scissors/rock_paper_scissors.py
@@ -64,37 +64,43 @@ STANCE_ART = {
         ],
     ],
 }
-player_score = 0
-ai_score = 0
 
-while player_score < 3 and ai_score < 3:
-    try:
-        player_stance, player_stance_value = STANCES[input("(r)ock, (p)aper, or (s)cissors? ")]
-    except KeyError:
-        continue
-    player_art = STANCE_ART[player_stance][0]
-    player_art_len = len(player_art[0])
 
-    ai_stance, ai_stance_value = choice(list(STANCES.values()))
-    ai_art = STANCE_ART[ai_stance][1]
-    ai_art_len = len(ai_art[0])
+def main():
+    player_score = 0
+    ai_score = 0
+    while player_score < 3 and ai_score < 3:
+        try:
+            player_stance, player_stance_value = STANCES[input("(r)ock, (p)aper, or (s)cissors? ")]
+        except KeyError:
+            continue
+        player_art = STANCE_ART[player_stance][0]
+        player_art_len = len(player_art[0])
 
-    centre = player_art_len * 2 + 2
+        ai_stance, ai_stance_value = choice(list(STANCES.values()))
+        ai_art = STANCE_ART[ai_stance][1]
+        ai_art_len = len(ai_art[0])
 
-    print(f"\n{'Player:':^{player_art_len}}  {'Computer:':^{ai_art_len}}")
-    print(f"{player_stance.title():^{player_art_len}}  {ai_stance.title():^{ai_art_len}}")
-    for player_stance_line, ai_stance_line in zip(player_art, ai_art):
-        print(f"{player_stance_line}  {ai_stance_line}")
+        centre = player_art_len * 2 + 2
 
-    if (player_stance_value + 1) % 3 == ai_stance_value:
-        print(f"\n{'Computer wins':^{centre}}")
-        ai_score += 1
-    elif player_stance_value == ai_stance_value:
-        print(f"\n{'Draw':^{centre}}")
-    else:
-        print(f"\n{'Player wins':^{centre}}")
-        player_score += 1
+        print(f"\n{'Player:':^{player_art_len}}  {'Computer:':^{ai_art_len}}")
+        print(f"{player_stance.title():^{player_art_len}}  {ai_stance.title():^{ai_art_len}}")
+        for player_stance_line, ai_stance_line in zip(player_art, ai_art):
+            print(f"{player_stance_line}  {ai_stance_line}")
 
-    print(f"{str(player_score) + ' - ' + str(ai_score):^{centre}}\n")
+        if (player_stance_value + 1) % 3 == ai_stance_value:
+            print(f"\n{'Computer wins':^{centre}}")
+            ai_score += 1
+        elif player_stance_value == ai_stance_value:
+            print(f"\n{'Draw':^{centre}}")
+        else:
+            print(f"\n{'Player wins':^{centre}}")
+            player_score += 1
 
-print("Thanks for playing!")
+        print(f"{str(player_score) + ' - ' + str(ai_score):^{centre}}\n")
+
+    print("Thanks for playing!")
+
+
+if __name__ == "__main__":
+    main()
